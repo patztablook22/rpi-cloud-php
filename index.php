9a7ef7f0857c4552aef2550269d1bb9af86f28ba
patztablook22 <patz@tuta.io>
2021-01-18 00:58:34 +0100

    Gitised

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once "includes/browser.inc";
+require_once "includes/config.inc"; 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+</head>
+  <?php include "includes/head.inc"; ?>
+<body>
+
+  <nav>
+    /<?= $browser->path ?>
+  </nav>
+
+  <main>
+    <?= $browser->show() ?>
+  </main>
+
+</body>
+</html>
